0fbc3c40fd7cea5c72ee598837b13f34b07a9461
Charles <charles.piraprez@student.unamur.be>
2019-07-18 11:01:01 +0200

    travail du 17/07

--- java/src/ernest.java
+++ java/src/ernest.java
@@ -33,6 +33,8 @@ public class ernest {
 			  System.out.println("something went wrong" + e.getMessage());
 		  }
 		  
+		  
+		  
 		}
 
 }
---
+++ java/src/tests/try_rest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+
+public class try_rest {
+
+	
+	
+}
